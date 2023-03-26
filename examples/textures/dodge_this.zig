968dee74ba2c12d8de29d0894371af326c183725
Jordan O'Leary <jdoleary@gmail.com>
2023-03-25 21:37:09 -0400

    src: Camera follows the hero

--- examples/textures/dodge_this.zig
+++ examples/textures/dodge_this.zig
@@ -28,18 +28,18 @@ const Unit = struct {
     velocity: rl.Vector2,
 };
 
-fn moveToTargetDynamicSpeed(self: *Unit, target:rl.Vector2) bool {
-    const speed = rlm.Lerp(0.0,20.0, rlm.Vector2Distance(self.pos, target)/300.0);
-    var bigA = target.x - self.pos.x;
-    var bigB = target.y - self.pos.y;
+fn moveToTargetDynamicSpeed(self: *rl.Vector2, target:rl.Vector2) bool {
+    const speed = rlm.Lerp(0.0,20.0, rlm.Vector2Distance(self.*, target)/300.0);
+    var bigA = target.x - self.x;
+    var bigB = target.y - self.y;
     var bigC = @sqrt(bigA*bigA + bigB*bigB);
     if(bigC < speed){
         return true;
     }
     var a = speed*bigA/bigC;
     var b = speed*bigB/bigC;
-    self.pos.x += a;
-    self.pos.y += b;
+    self.x += a;
+    self.y += b;
     // Haven't yet reached target
     return false;
 }
@@ -60,6 +60,14 @@ pub fn main() anyerror!void {
         .target = rl.Vector2{.x=0,.y=0},
         .velocity = rl.Vector2{.x=0,.y=0}
     };
+    var camera = rl.Camera2D {
+        .target = rl.Vector2 { .x = hero.pos.x, .y = hero.pos.y },
+        .offset = rl.Vector2 { .x = screenWidth/2, .y = screenHeight/2 },
+        .rotation = 0,
+        .zoom = 1,
+    };
+
+    rl.SetMousePosition(0,0);
 
     rl.SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -69,8 +77,6 @@ pub fn main() anyerror!void {
     {
         // Update
         //----------------------------------------------------------------------------------
-        const mousePos = rl.GetMousePosition();
-        _ = moveToTargetDynamicSpeed(&hero, mousePos);
 
         // Control frames speed
         // if (rl.IsKeyPressed(rl.KeyboardKey.KEY_RIGHT)) {framesSpeed+=1;}
@@ -85,14 +91,22 @@ pub fn main() anyerror!void {
         // Draw
         //----------------------------------------------------------------------------------
         rl.BeginDrawing();
+            camera.Begin();
+        const mousePosRaw = rl.GetMousePosition();
+        var mousePos = rl.GetScreenToWorld2D(mousePosRaw, camera);
+        std.debug.print("mouse: {d:.2} {d:.2}, {d:.2} {d:.2}\n", .{mousePos.x, mousePos.y, mousePosRaw.x, mousePosRaw.y});
+        _ = moveToTargetDynamicSpeed(&hero.pos, mousePos);
+        _ = moveToTargetDynamicSpeed(&camera.target, hero.pos);
 
             rl.ClearBackground(rl.RAYWHITE);
             if (c.GuiButton(.{ .x= 25, .y=255, .width=125, .height=30 }, "test")) {
                 std.debug.print("Button!\n", .{});
             }
+            // origin
+            rl.DrawCircle(0, 0, 2, rl.BLACK);
+        std.debug.print("mouse: {d},{d}\n", .{hero.pos.x, hero.pos.y});
             rl.DrawCircle(@floatToInt(c_int, hero.pos.x), @floatToInt(c_int,hero.pos.y), 16, rl.GREEN);
-
-
+            camera.End();
         rl.EndDrawing();
         //----------------------------------------------------------------------------------
     }
